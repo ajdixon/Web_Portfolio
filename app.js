b310fc926e87c23666e9347752b47a282fe6b667
ajdixon <alyssadixonj@gmail.com>
2023-09-23 20:38:43 -0400

    Cleaned up code and added code for different view points

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//scroll arrow
 window.addEventListener("scroll", function () {
     const scrollY = window.scrollY;
     if (scrollY > 0) {
@@ -11,13 +12,12 @@ window.addEventListener("scroll", function () {
     }
 });
 
+//project image appear function
 document.addEventListener("DOMContentLoaded", function() {
-    let images = document.querySelectorAll('.bb_img, .gallery_img');
-
+    let images = document.querySelectorAll('.mobile_img, .web_img');
     function isElementInViewport(el) {
         let rect = el.getBoundingClientRect();
         return (
-            rect.top >= 0 &&
             rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
         );
     }
@@ -31,13 +31,13 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         }
     }
-
     window.addEventListener('scroll', adjustOpacity);
     window.addEventListener('resize', adjustOpacity);
     window.addEventListener('load', adjustOpacity);
 });
 
-// Add this code to your JavaScript file (app.js)
+
+// Navbar scroll function
 document.addEventListener('DOMContentLoaded', function () {
     const navLinks = document.querySelectorAll('.nav_links a');
     
@@ -55,6 +55,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 const distance = targetPosition - startPosition;
                 const startTime = performance.now();
                 
+                
                 function scrollStep(timestamp) {
                     const currentTime = timestamp - startTime;
                     const scrollFraction = currentTime / duration;
@@ -69,5 +70,22 @@ document.addEventListener('DOMContentLoaded', function () {
                 requestAnimationFrame(scrollStep);
             }
         });
+    });
+});
+
+var nav_links = document.querySelector(".nav_links");
+var navIcon = document.getElementById("mobile-menu");
+var mobileLinks = document.querySelectorAll(".nav_links a"); // Select all links inside .nav_links
+
+navIcon.addEventListener("click", function() {
+    nav_links.classList.toggle("active");
+    navIcon.classList.toggle("active");
+});
+
+// Add event listeners to each link to hide the menu when clicked
+mobileLinks.forEach(function(link) {
+    link.addEventListener("click", function() {
+        nav_links.classList.remove("active"); // Remove "active" class to hide the menu
+        navIcon.classList.remove("active");   // Remove "active" class from the icon
     });
 });
